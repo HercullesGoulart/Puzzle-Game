3a8371df31f95e8a33aff81183584cb601e79021
viniciuspereira <viniciusp.faria@hotmail.com>
2019-02-26 00:29:43 -0300

    Player new animation

---
+++ Space Puzzle/Assets/_PuzzleGame/Scripts/PlayerAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private float DurationScaleUp = 0.02f;
+    [SerializeField] private float DurationScaleDown = 0.03f;    
+    [SerializeField] float playerJumpSpeed = .14f;
+    [SerializeField] float playerStrechOffset = 0.9f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        Vector3 upDirection = Vector3.up;
+
+        transform.DOLocalMoveY(1, playerJumpSpeed).SetLoops(-1, LoopType.Yoyo);
+        transform.DOBlendableLocalMoveBy(new Vector3(.2f, 0, 0), playerJumpSpeed * 2).SetLoops(-1, LoopType.Yoyo);
+
+        
+
+        float startDelay = playerJumpSpeed * 2;
+        InvokeRepeating("Strech", startDelay *playerStrechOffset, startDelay);
+
+    }
+
+    private void Strech()
+    {
+        Sequence s = DOTween.Sequence();
+        s.Append(transform.DOScale(new Vector3(1.1f, .7f, 1.1f), DurationScaleDown));
+        s.Insert(DurationScaleDown, transform.DOScale(new Vector3(1, 1f, 1), DurationScaleUp));
+    }
+
+}
