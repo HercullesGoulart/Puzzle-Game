159c2713501e8e3a1057804f4090f63d27fccea8
Herculles Goulart <hercullesbreno@gmail.com>
2019-03-15 23:05:09 -0300

    little changes, new falltile script

---
+++ Space Puzzle/Assets/_PuzzleGame/Scripts/DownTile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownTile : MonoBehaviour
+{
+    public Animator _anim;
+    public Tile tile;
+    public RestartLevel restart;
+    Collider Collider;
+
+    private void Start()
+    {
+        Collider = GetComponent<Collider>();
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            _anim.enabled = true;
+            StartCoroutine(DesactivateCol()); 
+
+        }
+
+    }
+    public void CurrentTile()
+    {
+        if (this.tile.current == true)
+        {
+            Debug.Log("currentTileTest");
+            StartCoroutine(delayfall());
+        }
+    }
+
+    public void DesactivateAnim()
+    {
+        _anim.enabled = false;
+        Collider.enabled = true;
+    }
+    IEnumerator delayfall()
+    {
+        yield return new WaitForSeconds(0.2f);
+        restart.TryAgain();
+    }
+    IEnumerator DesactivateCol()
+    {
+        yield return new WaitForSeconds(1.05f);
+        Collider.enabled = false;
+    }
+
+
+
+}
